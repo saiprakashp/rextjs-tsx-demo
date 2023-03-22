04d02373f7281c7577b7a18ac2e1c04cdb280216
Sai Prakash Pulipaka <pulipakasaiprakash@gmail.com>
2023-03-22 19:11:39 +0530

    Udpated Redux get

--- src/components/app/template/FilterComponent.tsx
+++ src/components/app/template/FilterComponent.tsx
@@ -34,7 +34,7 @@ const FilterComponent: React.FC = props => {
     const filterDataFunc = (event: React.ChangeEvent<HTMLInputElement>, keyData: string, valueData: string) => {
         if (filterCardData != null && filterCardData.data.get(keyData) !== undefined) {
             const data: Datafields | any = filterCardData.data.get(keyData);
-                data.showAccordian = true;
+            data.showAccordian = true;
             for (let i = 0; i < data.fields.length; i++) {
                 if (data.fields[i].keyText == valueData) {
                     data.fields[i].checked = event.target.checked;
@@ -76,6 +76,7 @@ const FilterComponent: React.FC = props => {
     useEffect(() => {
         if (filterData != null) {
             setFilterCardData(filterData);
+
         }
     }, [filterCardData]);
 
@@ -86,59 +87,67 @@ const FilterComponent: React.FC = props => {
             setFilterCardData({ ...filterCardData });
         }
     }
+    const generateComponent = () => {
+        let index = 0;
+        if (filterCardData != null)
+            for (let d of Array.from(filterCardData.data.keys())) {
+                index++
+                if (filterCardData.data.get(d) !== undefined) {
+                    let data: Datafields | any = filterCardData.data.get(d)
 
+                    return <Accordion sx={{ maxWidth: '20rem', bordershadow: '0', backgroundcolor: 'white', borderRadiou: '0' }}
+                        key={generateKey(data.key)} expanded={data.showAccordian}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon onClick={() => setAccordionOpen(data.key)} />}
+                            aria-controls={'panelt' + index}
+                            id={'panelt' + index}
+                        >
+                            <Typography>{data.title}</Typography>
+
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography  >
+                                <TextField
+                                    label="Search Text"
+                                    variant="standard"
+                                    onChange={(e: any) => searchFilter(e, data.key)}
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <SearchIcon />
+                                            </InputAdornment>
+                                        )
+                                    }}
+                                />
+                            </Typography>
+                            <Typography>
+                                <FormGroup>
+                                    {
+                                        Object.values(data.fields).map((fieldData: any, index) => {
+                                            return (
+                                                <FormControlLabel control={<Checkbox onClick={(e: any) =>
+                                                    filterDataFunc(e, data.key, fieldData.keyText)} checked={fieldData.checked} />}
+                                                    labelPlacement="end"
+                                                    label={fieldData.title + " (" + fieldData.chipText + ")"}
+                                                    key={generateKey(fieldData.keyText)}
+                                                />)
+                                        })
+                                    }
+                                </FormGroup>
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                }
+            }
+    }
     return (
         <>
 
             {
                 (filterCardData != null) ?
-                    [...filterCardData.data.keys()].map((d, index) => {
-                        if (filterCardData != null && filterCardData.data.get(d) !== undefined) {
-                            const data: Datafields | any = filterCardData.data.get(d);
-                            return <Accordion sx={{ maxWidth: '20rem', bordershadow: '0', backgroundcolor: 'white', borderRadiou: '0' }}
-                                key={generateKey(data.key)} expanded={data.showAccordian}>
-                                <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon onClick={() => setAccordionOpen(data.key)} />}
-                                    aria-controls={'panelt' + index}
-                                    id={'panelt' + index}
-                                >
-                                    <Typography>{data.title}</Typography>
 
-                                </AccordionSummary>
-                                <AccordionDetails>
-                                    <Typography  >
-                                        <TextField
-                                            label="Search Text"
-                                            variant="standard"
-                                            onChange={(e: any) => searchFilter(e, data.key)}
-                                            InputProps={{
-                                                startAdornment: (
-                                                    <InputAdornment position="start">
-                                                        <SearchIcon />
-                                                    </InputAdornment>
-                                                )
-                                            }}
-                                        />
-                                    </Typography>
-                                    <Typography>
-                                        <FormGroup>
-                                            {
-                                                Object.values(data.fields).map((fieldData: any, index) => {
-                                                    return (
-                                                        <FormControlLabel control={<Checkbox onClick={(e: any) =>
-                                                            filterDataFunc(e, data.key, fieldData.keyText)} checked={fieldData.checked} />}
-                                                            labelPlacement="end"
-                                                            label={fieldData.title + " (" + fieldData.chipText + ")"}
-                                                            key={generateKey(fieldData.keyText)}
-                                                        />)
-                                                })
-                                            }
-                                        </FormGroup>
-                                    </Typography>
-                                </AccordionDetails>
-                            </Accordion>
-                        }
-                    })
+                    generateComponent()
+
                     : <a> Loading</a>
             }
 
